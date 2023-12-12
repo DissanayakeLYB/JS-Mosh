a3708eb1b3b2f30012e1419facb115f12b99ad0a
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-12-12 21:22:15 +0530

    Create quadratic_solver.js

---
+++ quadratic_solver.js
@@ -0,0 +1,8 @@
+//this function gives the solutions for a quadratic equation
+
+function solutions(a,b,c){
+    console.log((-b+(((b**2)-4*a*c))**0.5)/2*a)
+    console.log((-b-(((b**2)-4*a*c))**0.5)/2*a)
+}
+
+solutions(1,-3,2)

ed58cba58ca53e585bb7b843d2b7611907c98512
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2023-12-24 21:55:48 +0530

    Create password_generator.js

---
+++ password_generator.js
@@ -0,0 +1,6 @@
+function password_gen(){
+    const letterArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    const numArr = ["0","1","2","3","4","5","6","7","8","9"]
+
+    
+}
